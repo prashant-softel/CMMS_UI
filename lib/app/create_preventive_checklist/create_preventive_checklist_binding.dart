0c783e64b0a437ced81d2a6d8bf85198f2b12b45
madhu <051msme@gmail.com>
2023-04-10 12:23:03 +0530

    drawer text fixed

--- lib/app/create_preventive_checklist/create_preventive_checklist_binding.dart
+++ lib/app/create_preventive_checklist/create_preventive_checklist_binding.dart
@@ -1,6 +1,9 @@
 import 'package:cmms/domain/usecases/create_preventivechecklist_usecase.dart';
 import 'package:get/get.dart';
 
+import '../../domain/usecases/home_usecase.dart';
+import '../home/home_controller.dart';
+import '../home/home_presenter.dart';
 import 'create_preventive_checklist_controller.dart';
 import 'create_preventive_checklist_presenter.dart';
 
@@ -12,6 +15,17 @@ class CreatePriventiveListBinding extends Bindings {
         Get.put(
           CreateCheckListPresenter(
             CreatePreventiveChecklistUsecase(
+              Get.find(),
+            ),
+          ),
+        ),
+      ),
+    );
+    Get.lazyPut(
+      () => HomeController(
+        Get.put(
+          HomePresenter(
+            HomeUsecase(
               Get.find(),
             ),
           ),
