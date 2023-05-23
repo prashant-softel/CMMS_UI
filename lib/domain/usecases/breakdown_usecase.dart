1e554b57a83ef89abfda44d8fc759db7f88df532
AdityaNawandar <nawandaraditya60@gmail.com>
2023-05-23 11:16:33 +0530

    Fix for screens getting loaded twice and change for ipa build

--- lib/domain/usecases/breakdown_usecase.dart
+++ lib/domain/usecases/breakdown_usecase.dart
@@ -3,10 +3,10 @@ import 'package:cmms/domain/domain.dart';
 import '../models/facility_model.dart';
 
 class BreakdownMaintenanceUsecase {
-   final Repository _repository;
+  final Repository _repository;
 
   BreakdownMaintenanceUsecase(this._repository);
 
-  Future<List<FacilityModel?>?> getFacilityList() async =>
-      await _repository.getFacilityList(true);
+  Future<List<FacilityModel?>?> getFacilityList(isLoading) async =>
+      await _repository.getFacilityList(isLoading);
 }
