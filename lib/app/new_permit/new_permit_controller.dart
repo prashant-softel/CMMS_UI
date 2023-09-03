76ac517524077cac6f7bdd2ad4f1d35b68913ebe
@anaszia <anaszia15@gmail.com>
2023-09-03 11:36:41 +0530

    permit changes

--- lib/app/new_permit/new_permit_controller.dart
+++ lib/app/new_permit/new_permit_controller.dart
@@ -961,6 +961,12 @@ class NewPermitController extends GetxController {
     if (selectedBlock.value == '') {
       isBlockSelected.value = false;
     }
+    if(selectedJobType.value == ''){
+      isJobTypeListSelected.value = false;
+    }
+    if(selectedSopPermit.value == ''){
+      isSopPermitListSelected.value = false;
+    }
     if (selectedTypePermit.value == '') {
       isTypePermit.value = false;
     }
@@ -988,6 +994,8 @@ class NewPermitController extends GetxController {
     if (isAssignedToSelected.value == false ||
         isFacilitySelected.value == false ||
         isBlockSelected.value == false ||
+        isJobTypeListSelected.value == false ||
+        isSopPermitListSelected.value == false ||
         isJobDescriptionInvalid == true) {
       isFormInvalid.value = true;
     } else {
