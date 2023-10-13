706b97f62e77c99827dd42ee43ceec053069df73
@anaszia <anaszia15@gmail.com>
2023-10-13 17:51:38 +0530

    hoto list screen design

---
+++ lib/app/hoto/hoto_list_presenter.dart
@@ -0,0 +1,23 @@
+import 'package:cmms/domain/models/facility_model.dart';
+import 'package:cmms/domain/models/module_cleaning_list_plan_model.dart';
+import 'package:cmms/domain/usecases/hoto_list_screen_usecase.dart';
+import 'package:cmms/domain/usecases/module_cleaning_list_plan_usecase.dart';
+
+class HotoListScreenPresenter {
+  HotoListScreenPresenter(this.hotoListUsecase);
+  HotoListScreenUsecase hotoListUsecase;
+
+  Future<List<FacilityModel?>?> getFacilityList({bool? isLoading}) async =>
+      await hotoListUsecase.getFacilityList(isLoading: isLoading);
+
+       Future<List<ModuleCleaningListPlanModel>> getModuleCleaningListPlan({
+    required bool isLoading,
+    required int? facility_id,
+  }) async {
+    return hotoListUsecase.getModuleCleaningListPlan(
+      isLoading: isLoading,
+      facility_id: facility_id,
+    );
+  }
+ 
+}
