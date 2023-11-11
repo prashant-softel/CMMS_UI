ca0f64d5234e39b15836375f8342aaa519b8d3c9
madhuban sahani <official051msme@gmail.com>
2023-11-11 13:13:41 +0530

    ocde pushed

--- lib/domain/models/file_model.dart
+++ lib/domain/models/file_model.dart
@@ -1,34 +1,34 @@
-import 'dart:html';
+// import 'dart:html';
 
-class FileModel {
-  String? url;
-  String? fileName;
-  String? mime;
-  int? bytes;
-  FileModel({
-    this.url,
-    this.fileName,
-    this.mime,
-    this.bytes,
-  });
-  String get size {
-    final kb = bytes ?? 0 / 1024;
-    final mb = kb / 1024;
-    return (mb > 1)
-        ? '${mb.toStringAsFixed(2)} MB'
-        : '${kb.toStringAsFixed(2)} KB';
-  }
+// class FileModel {
+//   String? url;
+//   String? fileName;
+//   String? mime;
+//   int? bytes;
+//   FileModel({
+//     this.url,
+//     this.fileName,
+//     this.mime,
+//     this.bytes,
+//   });
+//   String get size {
+//     final kb = bytes ?? 0 / 1024;
+//     final mb = kb / 1024;
+//     return (mb > 1)
+//         ? '${mb.toStringAsFixed(2)} MB'
+//         : '${kb.toStringAsFixed(2)} KB';
+//   }
 
-  File createHtmlFileFromModel(FileModel? model) {
-    // Create a blob from the bytes of the file
-    final blob = Blob([model?.bytes], model?.mime);
+//   File createHtmlFileFromModel(FileModel? model) {
+//     // Create a blob from the bytes of the file
+//     final blob = Blob([model?.bytes], model?.mime);
 
-    // Create a new file with the blob and filename
-    final file = File([blob], model?.fileName ?? "", {'type': model?.mime});
-    return file;
-    // Use the file as needed
-    // ...
-  }
+//     // Create a new file with the blob and filename
+//     final file = File([blob], model?.fileName ?? "", {'type': model?.mime});
+//     return file;
+//     // Use the file as needed
+//     // ...
+//   }
 
-  ///
-}
+//   ///
+// }
