2520c7e5c5b044290eece7cabe7a53d771db8550
madhuban sahani <official051msme@gmail.com>
2023-11-18 09:44:51 +0530

    pushed

--- lib/app/mis/view/mis_dashboard_screen.dart
+++ lib/app/mis/view/mis_dashboard_screen.dart
@@ -102,38 +102,6 @@ class MisDashboardScreen extends GetView<MisDashboardController> {
                       ),
                     if (Responsive.isDesktop(context))
 
-                    /// GRID TILES
-                    GridView.count(
-                      shrinkWrap: true,
-                      primary: false,
-                      padding: Dimens.edgeInsets15,
-                      crossAxisSpacing: 70,
-                      mainAxisSpacing: 6,
-                      crossAxisCount: Responsive.isMobile(context) ? 2 : 5,
-                      childAspectRatio: Responsive.isMobile(context)
-                          ? (itemWidth / itemHeight)
-                          : (itemWidth / itemHeightWeb),
-                      children: <Widget>[
-                        createContentTile(
-                          title: "List Of observation",
-                          onTap: () {
-                            controller.goToJobListScreen();
-                          },
-                        ),
-                        createContentTile(
-                          title: 'Create observation ',
-                          onTap: () {
-                            controller.goToCreateObservation();
-                          },
-                        ),
-                        createContentTile(
-                          title: 'Check List Of observation',
-                          onTap: () {
-                            controller.goToCheckListOfObservation();
-                          },
-                        )
-                      ],
-                    ),
                       /// GRID TILES
                       GridView.count(
                         shrinkWrap: true,
@@ -147,17 +115,50 @@ class MisDashboardScreen extends GetView<MisDashboardController> {
                             : (itemWidth / itemHeightWeb),
                         children: <Widget>[
                           createContentTile(
-                              title: "List Of observation",
-                              onTap: () {
-                                controller.goToJobListScreen();
-                              }),
+                            title: "List Of observation",
+                            onTap: () {
+                              controller.goToJobListScreen();
+                            },
+                          ),
                           createContentTile(
-                              title: 'Create observation ',
-                              onTap: () {
-                                controller.goToCreateObservation();
-                              })
+                            title: 'Create observation ',
+                            onTap: () {
+                              controller.goToCreateObservation();
+                            },
+                          ),
+                          createContentTile(
+                            title: 'Check List Of observation',
+                            onTap: () {
+                              controller.goToCheckListOfObservation();
+                            },
+                          )
                         ],
                       ),
+
+                    /// GRID TILES
+                    // GridView.count(
+                    //   shrinkWrap: true,
+                    //   primary: false,
+                    //   padding: Dimens.edgeInsets15,
+                    //   crossAxisSpacing: 70,
+                    //   mainAxisSpacing: 6,
+                    //   crossAxisCount: Responsive.isMobile(context) ? 2 : 5,
+                    //   childAspectRatio: Responsive.isMobile(context)
+                    //       ? (itemWidth / itemHeight)
+                    //       : (itemWidth / itemHeightWeb),
+                    //   children: <Widget>[
+                    //     createContentTile(
+                    //         title: "List Of observation",
+                    //         onTap: () {
+                    //           controller.goToJobListScreen();
+                    //         }),
+                    //     createContentTile(
+                    //         title: 'Create observation ',
+                    //         onTap: () {
+                    //           controller.goToCreateObservation();
+                    //         })
+                    //   ],
+                    // ),
                     if (Responsive.isDesktop(context))
                       Container(
                         margin: EdgeInsets.only(left: 20),
