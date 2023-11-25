bb40e4a7a86fbe7edfae80cbb3ce9d4c1a84c430
madhuban sahani <official051msme@gmail.com>
2023-11-25 14:23:30 +0530

    code pushed for the some aligment issue in the user bscreen and fixed the searching

--- lib/app/user_list/view/user_list_content_web.dart
+++ lib/app/user_list/view/user_list_content_web.dart
@@ -139,75 +139,77 @@ class _UserListContentWebState extends State<UserListContentWeb> {
                                           ),
                                         ),
                                       ),
-                                      itemBuilder: (BuildContext context) => <
-                                          PopupMenuEntry<String>>[]..addAll(
-                                            controller
-                                                .columnVisibility.value.entries
-                                                .map((e) {
-                                          return PopupMenuItem<String>(
-                                              child: ValueListenableBuilder(
-                                                  valueListenable: controller
-                                                      .columnVisibility,
-                                                  builder:
-                                                      (context, value, child) {
-                                                    return Row(
-                                                      children: [
-                                                        Checkbox(
-                                                          value: value[e.key],
-                                                          onChanged:
-                                                              (newValue) {
-                                                            controller
-                                                                .setColumnVisibility(
-                                                                    e.key,
-                                                                    newValue!);
-                                                          },
-                                                        ),
-                                                        Text(e.key),
-                                                      ],
-                                                    );
-                                                  }));
-                                        })),
+                                      itemBuilder: (BuildContext context) =>
+                                          <PopupMenuEntry<String>>[]..addAll(
+                                                controller.columnVisibility
+                                                    .value.entries
+                                                    .map((e) {
+                                              return PopupMenuItem<String>(
+                                                  child: ValueListenableBuilder(
+                                                      valueListenable:
+                                                          controller
+                                                              .columnVisibility,
+                                                      builder: (context, value,
+                                                          child) {
+                                                        return Row(
+                                                          children: [
+                                                            Checkbox(
+                                                              value:
+                                                                  value[e.key],
+                                                              onChanged:
+                                                                  (newValue) {
+                                                                controller
+                                                                    .setColumnVisibility(
+                                                                        e.key,
+                                                                        newValue!);
+                                                              },
+                                                            ),
+                                                            Text(e.key),
+                                                          ],
+                                                        );
+                                                      }));
+                                            })),
                                       onSelected: (String value) {
                                         // Handle column selection
                                       },
                                     ),
-                                    Container(
-                                      height: 35,
-                                      margin: EdgeInsets.only(left: 10),
-                                      child: CustomElevatedButton(
-                                        backgroundColor:
-                                            ColorValues.appLightBlueColor,
-                                        onPressed: () {},
-                                        text: 'Column Visibility',
-                                      ),
-                                    ),
-                                    Container(
-                                      height: 35,
-                                      margin: EdgeInsets.only(left: 10),
-                                      child: CustomElevatedButton(
-                                          backgroundColor:
-                                              ColorValues.appLightBlueColor,
-                                          onPressed: () {},
-                                          text: 'Copy'),
-                                    ),
-                                    Container(
-                                      height: 35,
-                                      margin: EdgeInsets.only(left: 10),
-                                      child: CustomElevatedButton(
-                                          backgroundColor:
-                                              ColorValues.appLightBlueColor,
-                                          onPressed: () {},
-                                          text: 'Excel'),
-                                    ),
-                                    Container(
-                                      height: 35,
-                                      margin: EdgeInsets.only(left: 10),
-                                      child: CustomElevatedButton(
-                                          backgroundColor:
-                                              ColorValues.appLightBlueColor,
-                                          onPressed: () {},
-                                          text: 'PDF'),
-                                    ),
+                                    // Container(
+                                    //   height: 35,
+                                    //   margin: EdgeInsets.only(left: 10),
+                                    //   child: CustomElevatedButton(
+                                    //     backgroundColor:
+                                    //         ColorValues.appLightBlueColor,
+                                    //     onPressed: () {},
+                                    //     text: 'Column Visibility',
+                                    //   ),
+                                    // ),
+                                    // Container(
+                                    //   height: 35,
+                                    //   margin: EdgeInsets.only(left: 10),
+                                    //   child: CustomElevatedButton(
+                                    //       backgroundColor:
+                                    //           ColorValues.appLightBlueColor,
+                                    //       onPressed: () {},
+                                    //       text: 'Copy'),
+                                    // ),
+                                    // Container(
+                                    //   height: 35,
+                                    //   margin: EdgeInsets.only(left: 10),
+                                    //   child: CustomElevatedButton(
+                                    //       backgroundColor:
+                                    //           ColorValues.appLightBlueColor,
+                                    //       onPressed: () {},
+                                    //       text: 'Excel'),
+                                    // ),
+                                    // Container(
+                                    //   height: 35,
+                                    //   margin: EdgeInsets.only(left: 10),
+                                    //   child: CustomElevatedButton(
+                                    //       backgroundColor:
+                                    //           ColorValues.appLightBlueColor,
+                                    //       onPressed: () {},
+                                    //       text: 'PDF'),
+                                    // ),
                                     Spacer(),
                                     Container(
                                       width: 200,
