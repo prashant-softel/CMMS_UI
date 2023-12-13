35108e8c02218bd46524a2630fce2820febec2c1
Rukmini Sahani <139337361+cmmstest@users.noreply.github.com>
2023-12-13 21:07:27 +0530

    code pushed

--- lib/app/create_pm_plan/view/create_pm_plan_web.dart
+++ lib/app/create_pm_plan/view/create_pm_plan_web.dart
@@ -78,7 +78,7 @@ class _CreatePmPlanWebState extends State<CreatePmPlanWeb> {
                     child: Stack(
                       children: [
                         Container(
-                          height: Get.height,
+                          // height: Get.height,
                           child: Card(
                             margin: EdgeInsets.all(20),
                             color: ColorValues.whiteColor,
@@ -317,141 +317,132 @@ class _CreatePmPlanWebState extends State<CreatePmPlanWeb> {
                                 Dimens.boxHeight12,
                                 // controller.filteredInventoryNameList.length > 0
                                 //     ?
-                                Expanded(
-                                  child: Container(
-                                    margin: Dimens.edgeInsets20,
-                                    height: 600,
-                                    //  ((controller
-                                    //             .filteredInventoryNameList
-                                    //             .length) *
-                                    //         30) +
-                                    //     100,
-                                    decoration: BoxDecoration(
-                                      border: Border.all(
-                                        color: ColorValues
-                                            .lightGreyColorWithOpacity35,
-                                        width: 1,
+                                Container(
+                                  margin: Dimens.edgeInsets20,
+                                  height: //50,
+                                      //  height: 200,
+                                      ((controller.filteredInventoryNameList
+                                                  .length) *
+                                              30) +
+                                          100,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: ColorValues
+                                          .lightGreyColorWithOpacity35,
+                                      width: 1,
+                                    ),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color:
+                                            ColorValues.appBlueBackgroundColor,
+                                        spreadRadius: 2,
+                                        blurRadius: 5,
+                                        offset: Offset(0, 2),
                                       ),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: ColorValues
-                                              .appBlueBackgroundColor,
-                                          spreadRadius: 2,
-                                          blurRadius: 5,
-                                          offset: Offset(0, 2),
+                                    ],
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      Padding(
+                                        padding: const EdgeInsets.all(10.0),
+                                        child: Row(
+                                          children: [
+                                            Text(
+                                              "Equipment List ",
+                                              style: Styles.blue700,
+                                            ),
+                                          ],
                                         ),
-                                      ],
-                                    ),
-                                    child: Column(
-                                      children: [
-                                        Padding(
-                                          padding: const EdgeInsets.all(10.0),
-                                          child: Row(
-                                            children: [
-                                              Text(
-                                                "Equipment List ",
-                                                style: Styles.blue700,
-                                              ),
-                                            ],
-                                          ),
+                                      ),
+                                      Expanded(
+                                        child: DataTable2(
+                                          border: TableBorder.all(
+                                              color: Color.fromARGB(
+                                                  255, 206, 229, 234)),
+                                          columns: [
+                                            DataColumn2(
+                                                //fixedWidth: 150,
+                                                label: Text(
+                                              'Assest name',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                            DataColumn2(
+                                                fixedWidth: 150,
+                                                label: Text(
+                                                  'Assest ID',
+                                                  style: TextStyle(
+                                                      fontSize: 15,
+                                                      fontWeight:
+                                                          FontWeight.bold),
+                                                )),
+                                            DataColumn2(
+                                                // fixedWidth: 300,
+                                                label: Text(
+                                              'Parent Assest',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                            DataColumn2(
+                                                // fixedWidth: 300,
+                                                label: Text(
+                                              'Module Quantity',
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                            DataColumn2(
+                                                // fixedWidth: 300,
+                                                label: Text(
+                                              "Checklist",
+                                              style: TextStyle(
+                                                  fontSize: 15,
+                                                  fontWeight: FontWeight.bold),
+                                            )),
+                                          ],
+                                          rows: controller.rowItem.value
+                                              .map((record) {
+                                            return DataRow(
+                                              // height: 130,
+                                              cells: record.map((mapData) {
+                                                return DataCell(
+                                                  (mapData['key'] ==
+                                                          "checklist")
+                                                      ? DropdownWebStock(
+                                                          width: MediaQuery.of(
+                                                                  context)
+                                                              .size
+                                                              .width,
+                                                          dropdownList: controller
+                                                              .preventiveCheckList,
+                                                          selectedValue:
+                                                              mapData["value"],
+                                                          onValueChanged: (list,
+                                                              selectedValue) {
+                                                            mapData["value"] =
+                                                                selectedValue;
+                                                            controller.checkdropdownMapperData[
+                                                                    selectedValue] =
+                                                                list.firstWhere(
+                                                                    (element) =>
+                                                                        element
+                                                                            .name ==
+                                                                        selectedValue,
+                                                                    orElse:
+                                                                        null);
+                                                          },
+                                                        )
+                                                      : Text(mapData['value'] ??
+                                                          ''),
+                                                );
+                                              }).toList(),
+                                            );
+                                          }).toList(),
                                         ),
-                                        Expanded(
-                                          child: DataTable2(
-                                            border: TableBorder.all(
-                                                color: Color.fromARGB(
-                                                    255, 206, 229, 234)),
-                                            columns: [
-                                              DataColumn2(
-                                                  //fixedWidth: 150,
-                                                  label: Text(
-                                                'Assest name',
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              )),
-                                              DataColumn2(
-                                                  fixedWidth: 150,
-                                                  label: Text(
-                                                    'Assest ID',
-                                                    style: TextStyle(
-                                                        fontSize: 15,
-                                                        fontWeight:
-                                                            FontWeight.bold),
-                                                  )),
-                                              DataColumn2(
-                                                  // fixedWidth: 300,
-                                                  label: Text(
-                                                'Parent Assest',
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              )),
-                                              DataColumn2(
-                                                  // fixedWidth: 300,
-                                                  label: Text(
-                                                'Module Quantity',
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              )),
-                                              DataColumn2(
-                                                  // fixedWidth: 300,
-                                                  label: Text(
-                                                "Checklist",
-                                                style: TextStyle(
-                                                    fontSize: 15,
-                                                    fontWeight:
-                                                        FontWeight.bold),
-                                              )),
-                                            ],
-                                            rows: controller.rowItem.value
-                                                .map((record) {
-                                              return DataRow(
-                                                // height: 130,
-                                                cells: record.map((mapData) {
-                                                  return DataCell(
-                                                    (mapData['key'] ==
-                                                            "checklist")
-                                                        ? DropdownWebStock(
-                                                            width:
-                                                                MediaQuery.of(
-                                                                        context)
-                                                                    .size
-                                                                    .width,
-                                                            dropdownList: controller
-                                                                .preventiveCheckList,
-                                                            selectedValue:
-                                                                mapData[
-                                                                    "value"],
-                                                            onValueChanged: (list,
-                                                                selectedValue) {
-                                                              mapData["value"] =
-                                                                  selectedValue;
-                                                              controller.checkdropdownMapperData[
-                                                                      selectedValue] =
-                                                                  list.firstWhere(
-                                                                      (element) =>
-                                                                          element
-                                                                              .name ==
-                                                                          selectedValue,
-                                                                      orElse:
-                                                                          null);
-                                                            },
-                                                          )
-                                                        : Text(
-                                                            mapData['value'] ??
-                                                                ''),
-                                                  );
-                                                }).toList(),
-                                              );
-                                            }).toList(),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                                 //  : Dimens.box0,
@@ -459,13 +450,13 @@ class _CreatePmPlanWebState extends State<CreatePmPlanWeb> {
                                 Row(
                                   children: [
                                     Spacer(),
-                                    CustomElevatedButton(
-                                      backgroundColor: ColorValues.appRedColor,
-                                      text: 'cancel',
-                                      onPressed: () {
-                                        // controller.AddInventory();
-                                      },
-                                    ),
+                                    // CustomElevatedButton(
+                                    //   backgroundColor: ColorValues.appRedColor,
+                                    //   text: 'cancel',
+                                    //   onPressed: () {
+                                    //     // controller.AddInventory();
+                                    //   },
+                                    // ),
                                     Dimens.boxWidth15,
                                     controller.pmPlanId != 0
                                         ? CustomElevatedButton(
