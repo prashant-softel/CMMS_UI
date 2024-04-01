05f4b9bf406886aecd9e1f76a296b9fa1fd8896a
Rukmini Sahani <139337361+cmmstest@users.noreply.github.com>
2024-04-01 11:03:41 +0530

    tbt done

--- lib/app/view_permit/view_permit_controller.dart
+++ lib/app/view_permit/view_permit_controller.dart
@@ -479,8 +479,10 @@ class ViewPermitController extends GetxController {
         print('FacilityIdsss$facilityId');
         Future.delayed(Duration(seconds: 1), () async {
           await getBlocksList(facilityId);
-          await getViewPermitDetail(permitId: permitId.value, facilityId: facilityId);
-          await getPermitHistory(permitId: permitId.value, facilityId: facilityId);
+          await getViewPermitDetail(
+              permitId: permitId.value, facilityId: facilityId);
+          await getPermitHistory(
+              permitId: permitId.value, facilityId: facilityId);
         });
         // Future.delayed(Duration(seconds: 1), () {
 
@@ -488,7 +490,7 @@ class ViewPermitController extends GetxController {
       });
       // await getViewPermitDetail(
       //     permitId: permitId.value, facilityId: facilityId);
-      await getSafetyMeasureList();
+      // await getSafetyMeasureList();
       await getPermitHistory(permitId: permitId.value, facilityId: facilityId);
       await getPermitConditionList(isCancle: isCancle!, facilityId: facilityId);
       await getPermitCloseConditionList(isClose: isClose!);
@@ -844,6 +846,9 @@ class ViewPermitController extends GetxController {
           '${DateFormat('yyyy-MM-dd HH:mm').format(DateTime.parse('${viewPermitDetailsModel.value?.end_datetime}'))}';
 
       selectedSafetyMeasureId = viewPermitDetailsModel.value?.permitTypeid ?? 0;
+      if (selectedSafetyMeasureId > 0) {
+        getSafetyMeasureList();
+      }
       rowTBTTrainingOtherPersonItem.value = [];
       viewPermitDetailsModel.value?.lotoOtherDetails?.forEach((element) {
         rowTBTTrainingOtherPersonItem.value.add([
@@ -961,6 +966,8 @@ class ViewPermitController extends GetxController {
       permit_type_id: selectedSafetyMeasureId,
       // job_type_id: 36,
     );
+    safetyMeasureList.value = <SafetyMeasureListModel>[];
+
     print('View SafetyrMeasure${selectedSafetyMeasureId}');
     for (var safetyMeasure_list in _safetyMeasureList) {
       safetyMeasureList.add(safetyMeasure_list);
