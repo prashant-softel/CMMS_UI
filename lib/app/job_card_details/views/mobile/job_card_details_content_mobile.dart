9dca0843a946defba3a26433bb0f12aa9c9bb5fd
Rukmini Sahani <139337361+cmmstest@users.noreply.github.com>
2024-06-22 10:49:50 +0530

    job and link to permit

--- lib/app/job_card_details/views/mobile/job_card_details_content_mobile.dart
+++ lib/app/job_card_details/views/mobile/job_card_details_content_mobile.dart
@@ -42,189 +42,189 @@ class JobCardDetailsContentMobile extends GetView<JobCardDetailsController> {
           ),
         ),
         child: //
-            Obx(
-          () => //
+            //   Obx(
+            // () => //
 
-              SingleChildScrollView(
-            child: //
-                Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    //
+            SingleChildScrollView(
+          child: //
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  //
+                  children: [
+                /// TABLE - PLANT DETAILS
+                Row(//
                     children: [
-                  /// TABLE - PLANT DETAILS
-                  Row(//
-                      children: [
-                    Text('Plant Details', style: Styles.blackBold16),
-                  ]),
-                  TransposedTable(controller.plantDetails),
-                  Dimens.boxHeight20,
-
-                  /// TABLE - JOB DETAILS
-                  Row(//
-                      children: [
-                    Text('Job Details', style: Styles.blackBold16),
-                  ]),
-                  TransposedTable(controller.jobDetails),
-                  Dimens.boxHeight20,
-
-                  /// TABLE - PERMIT DETAILS
-                  Row(//
-                      children: [
-                    Text('Permit Details', style: Styles.blackBold16),
-                  ]),
-                  TransposedTable(controller.permitDetails),
-                  Dimens.boxHeight20,
-
-                  /// ISOLATED ASSETS TABLE WIDGET
-                  (controller.isolationAssetsCategoryList.isNotEmpty)
-                      ? IsolatedAssetsWidget()
-                      : Dimens.boxHeight0,
-
-                  /// LOTO APPLIED ASSETS TABLE WIDGET
-                  (controller.lotoAppliedAssets.isNotEmpty)
-                      ? LotoAppliedAssetsWidget()
-                      : Dimens.boxHeight20,
-                  CustomDivider(),
-
-                  /// EMPLOYEE TABLE
-                  EmployeeTableWidget(
-                    isWeb: false,
-                    controller: controller,
-                  ),
-                  Dimens.boxHeight20,
-                  CustomDivider(),
-
-                  /// FILE UPLOAD WIDGET
-                  (Text('File Upload', style: Styles.blackBold16)),
-                  Container(
-                    width: Get.width,
-                    child: //
-                        Center(
-                      child: Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center, //
-                          children: [
-                            FileUploadWidgetWithDropzone(),
-                            Dimens.boxHeight10,
-                            Flexible(child: FileUploadDetailsWidgetMobile()),
-                          ]),
-                    ),
-                  ),
-                  Dimens.boxHeight10,
-                  CustomDivider(),
-
-                  ///HISTORY
-                  Text('History', style: Styles.blackBold16),
-                  (controller.isJobCardStarted.value == true &&
-                          controller.historyList != null &&
-                          controller.historyList!.isNotEmpty)
-                      ? //
-                      Container(
-                          constraints: BoxConstraints(
-                            maxHeight: Get.height * 0.4,
-                            minHeight: 100,
-                          ),
-                          child: //
-                              HistoryTableWidgetMobile(
-                            historyList: controller.historyList,
-                          ),
-                        )
-                      //)
-                      : //
-                      Text('No Data'),
-                  Dimens.boxHeight10,
-
-                  /// DESCRIPTION OF WORK DONE
-                  Wrap(
-                      //
-
-                      children: [
-                        Text('Description of work done: '),
-                        Expanded(
-                          child: TextField(
-                            style: GoogleFonts.lato(
-                              textStyle: TextStyle(
-                                  fontSize: 16.0,
-                                  height: 1.0,
-                                  color: Colors.black),
-                            ),
-                            controller: controller.descriptionOfWorkDoneCtrlr,
-                            enabled: controller.isJobCardStarted.value,
-                            decoration: InputDecoration(
-                              disabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: ColorValues.appLightGreyColor,
-                                  width: 1.0,
-                                ),
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: ColorValues.appLightBlueColor,
-                                  width: 1.0,
-                                ),
-                              ),
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: ColorValues.appLightBlueColor,
-                                  width: 1.0,
-                                ),
-                              ),
-                            ),
-                            keyboardType: TextInputType.multiline,
-                            minLines: 5,
-                            maxLines: null,
-                          ),
-                        ),
-                      ]),
-                  Dimens.boxHeight20,
-
-                  /// START JOB CARD BUTTON
-                  (controller.isJobCardStarted == false)
-                      ? //
-                      Row(
-                          mainAxisAlignment: MainAxisAlignment.center, //
-                          children: [
-                              CustomElevatedButton(
-                                text: 'Start Job Card',
-                                onPressed: () {},
-                                // => controller.createJobCard(),
-                                backgroundColor: ColorValues.appGreenColor,
-                              ),
-                              Dimens.boxWidth10,
-                            ])
-                      : //
-                      Wrap(children: [
-                          CustomElevatedButton(
-                            text: 'Cancel',
-                            onPressed: () => controller.startStopJobCard(),
-                            backgroundColor: ColorValues.appRedColor,
-                          ),
-                          Dimens.boxWidth10,
-                          CustomElevatedButton(
-                            text: 'Update',
-                            onPressed: () => controller.updateJobCard(),
-                            backgroundColor: ColorValues.appYellowColor,
-                          ),
-                          Dimens.boxWidth10,
-                          CustomElevatedButton(
-                            text: 'Close Job',
-                            onPressed: () => controller.startStopJobCard(),
-                            backgroundColor: ColorValues.appGreenColor,
-                          ),
-                          Dimens.boxWidth10,
-                          CustomElevatedButton(
-                            text: 'Carry Forward Job',
-                            onPressed: () => controller.carryForwardJob(),
-                            backgroundColor: ColorValues.appLightBlueColor,
-                          ),
-                          Dimens.boxWidth10,
-                        ]),
-                  Dimens.boxHeight20,
+                  Text('Plant Details', style: Styles.blackBold16),
                 ]),
-          ),
+                TransposedTable(controller.plantDetails),
+                Dimens.boxHeight20,
+
+                /// TABLE - JOB DETAILS
+                Row(//
+                    children: [
+                  Text('Job Details', style: Styles.blackBold16),
+                ]),
+                TransposedTable(controller.jobDetails),
+                Dimens.boxHeight20,
+
+                /// TABLE - PERMIT DETAILS
+                Row(//
+                    children: [
+                  Text('Permit Details', style: Styles.blackBold16),
+                ]),
+                TransposedTable(controller.permitDetails),
+                Dimens.boxHeight20,
+
+                /// ISOLATED ASSETS TABLE WIDGET
+                (controller.isolationAssetsCategoryList.isNotEmpty)
+                    ? IsolatedAssetsWidget()
+                    : Dimens.boxHeight0,
+
+                /// LOTO APPLIED ASSETS TABLE WIDGET
+                (controller.lotoAppliedAssets.isNotEmpty)
+                    ? LotoAppliedAssetsWidget()
+                    : Dimens.boxHeight20,
+                CustomDivider(),
+
+                /// EMPLOYEE TABLE
+                EmployeeTableWidget(
+                  isWeb: false,
+                  controller: controller,
+                ),
+                Dimens.boxHeight20,
+                CustomDivider(),
+
+                /// FILE UPLOAD WIDGET
+                (Text('File Upload', style: Styles.blackBold16)),
+                Container(
+                  width: Get.width,
+                  child: //
+                      Center(
+                    child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center, //
+                        children: [
+                          FileUploadWidgetWithDropzone(),
+                          Dimens.boxHeight10,
+                          Flexible(child: FileUploadDetailsWidgetMobile()),
+                        ]),
+                  ),
+                ),
+                Dimens.boxHeight10,
+                CustomDivider(),
+
+                ///HISTORY
+                Text('History', style: Styles.blackBold16),
+                (controller.isJobCardStarted.value == true &&
+                        controller.historyList != null &&
+                        controller.historyList!.isNotEmpty)
+                    ? //
+                    Container(
+                        constraints: BoxConstraints(
+                          maxHeight: Get.height * 0.4,
+                          minHeight: 100,
+                        ),
+                        child: //
+                            HistoryTableWidgetMobile(
+                          historyList: controller.historyList,
+                        ),
+                      )
+                    //)
+                    : //
+                    Text('No Data'),
+                Dimens.boxHeight10,
+
+                /// DESCRIPTION OF WORK DONE
+                Wrap(
+                    //
+
+                    children: [
+                      Text('Description of work done: '),
+                      Expanded(
+                        child: TextField(
+                          style: GoogleFonts.lato(
+                            textStyle: TextStyle(
+                                fontSize: 16.0,
+                                height: 1.0,
+                                color: Colors.black),
+                          ),
+                          controller: controller.descriptionOfWorkDoneCtrlr,
+                          enabled: controller.isJobCardStarted.value,
+                          decoration: InputDecoration(
+                            disabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: ColorValues.appLightGreyColor,
+                                width: 1.0,
+                              ),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: ColorValues.appLightBlueColor,
+                                width: 1.0,
+                              ),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                color: ColorValues.appLightBlueColor,
+                                width: 1.0,
+                              ),
+                            ),
+                          ),
+                          keyboardType: TextInputType.multiline,
+                          minLines: 5,
+                          maxLines: null,
+                        ),
+                      ),
+                    ]),
+                Dimens.boxHeight20,
+
+                /// START JOB CARD BUTTON
+                (controller.isJobCardStarted == false)
+                    ? //
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center, //
+                        children: [
+                            CustomElevatedButton(
+                              text: 'Start Job Card',
+                              onPressed: () {},
+                              // => controller.createJobCard(),
+                              backgroundColor: ColorValues.appGreenColor,
+                            ),
+                            Dimens.boxWidth10,
+                          ])
+                    : //
+                    Wrap(children: [
+                        CustomElevatedButton(
+                          text: 'Cancel',
+                          onPressed: () => controller.startStopJobCard(),
+                          backgroundColor: ColorValues.appRedColor,
+                        ),
+                        Dimens.boxWidth10,
+                        CustomElevatedButton(
+                          text: 'Update',
+                          onPressed: () => controller.updateJobCard(),
+                          backgroundColor: ColorValues.appYellowColor,
+                        ),
+                        Dimens.boxWidth10,
+                        CustomElevatedButton(
+                          text: 'Close Job',
+                          onPressed: () => controller.startStopJobCard(),
+                          backgroundColor: ColorValues.appGreenColor,
+                        ),
+                        Dimens.boxWidth10,
+                        CustomElevatedButton(
+                          text: 'Carry Forward Job',
+                          onPressed: () => controller.carryForwardJob(),
+                          backgroundColor: ColorValues.appLightBlueColor,
+                        ),
+                        Dimens.boxWidth10,
+                      ]),
+                Dimens.boxHeight20,
+              ]),
         ),
       ),
+      //  ),
     );
   }
 }
