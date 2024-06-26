429e5817a71f9d02842948c56a525d088ae6bd58
Hasti Shah <hastispammz@gmail.com>
2024-06-26 13:23:00 +0530

    routing reverted

--- lib/app/pm_task_view/pm_task_view_controller.dart
+++ lib/app/pm_task_view/pm_task_view_controller.dart
@@ -409,7 +409,7 @@ class PreventiveMaintenanceTaskViewController extends GetxController {
   }
 
   Future<void> gotoexecution() async {
-    Get.offAllNamed(Routes.pmExecution, arguments: {'pmTaskId': scheduleId.value});
+    Get.offNamed(Routes.pmExecution, arguments: {'pmTaskId': scheduleId.value});
   }
 
   createNewPermit() {
