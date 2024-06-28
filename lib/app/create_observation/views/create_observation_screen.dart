d1ffc0469dccef1cd5b5eb26a5a9ec0df83f0c2f
madhubansahani <107603778+madhubansahani@users.noreply.github.com>
2024-06-28 10:20:18 +0530

    done obs changes

--- lib/app/create_observation/views/create_observation_screen.dart
+++ lib/app/create_observation/views/create_observation_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cmms/app/create_observation/views/mobile/create_observation_mobile.dart';
 import 'package:cmms/app/home/home_controller.dart';
+import 'package:cmms/app/home/widgets/heading_profile_app_bar.dart';
+import 'package:cmms/app/home/widgets/mobile_drawer.dart';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -12,56 +14,59 @@ import 'web/create_observation_web.dart';
 class CreateObservationScreen extends GetView<CreateObservationController> {
   CreateObservationScreen({super.key});
   final controller = Get.find<CreateObservationController>();
-  final HomeController homecontroller = Get.find();
+  final homecontroller = Get.find<HomeController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Responsive.isMobile(context)
           ? AppBar(
-              centerTitle: true,
-              elevation: 0,
+              title: HeadingProfileAppBar(
+                title: "List Of Observation",
+              ),
             )
           : null,
       drawer: //
           (Responsive.isMobile(context) || Responsive.isTablet(context))
-              ? HomeDrawer()
+              ? HomeDrawerMobile()
               : null,
-      body: Obx(
-        () => Stack(
-          children: [
-            AnimatedContainer(
-                duration: Duration(milliseconds: 450),
-                margin: EdgeInsets.only(
-                    left: homecontroller.menuButton.value ? 250.0 : 70.0),
-                width: Get.width,
-                height: Get.height,
-                child: Row(
-                  children: [
-                    (Responsive.isMobile(context) ||
-                            Responsive.isTablet(context))
-                        ? Dimens.box0
-                        : Container(),
-                    Expanded(
-                      child: Column(
-                        children: [
-                          if (Responsive.isMobile(context))
-                            Expanded(
-                              child: CreateObservationMobile(),
-                            ),
-                          if (Responsive.isDesktop(context))
-                            Expanded(
-                              child: CreateObservationWeb(),
-                            )
-                        ],
-                      ),
+      body: Stack(
+        children: [
+          AnimatedContainer(
+              duration: Duration(milliseconds: 450),
+              margin: EdgeInsets.only(
+                left: Responsive.isDesktop(context)
+                    ? homecontroller.menuButton.value
+                        ? 250.0
+                        : 70.0
+                    : 0,
+              ),
+              width: Get.width,
+              height: Get.height,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        if (Responsive.isMobile(context))
+                          Expanded(
+                            child: CreateObservationMobile(),
+                          ),
+                        if (Responsive.isDesktop(context))
+                          Expanded(
+                            child: CreateObservationWeb(),
+                          )
+                      ],
                     ),
-                  ],
-                )),
-            AnimatedPositioned(
-                child: HomeDrawer(), duration: Duration(milliseconds: 450))
-          ],
-        ),
+                  ),
+                ],
+              )),
+          Responsive.isDesktop(context)
+              ? AnimatedPositioned(
+                  child: HomeDrawer(), duration: Duration(milliseconds: 450))
+              : Dimens.box0
+        ],
+        //  ),
       ),
     );
   }
