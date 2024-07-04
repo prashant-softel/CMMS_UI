55f550733438e417aa45a2e525103c3a5f9cbbf2
SONAWANE TANMAY VIVEK <fs21co023.tanmay@gmail.com>
2024-07-04 15:11:18 +0530

    view vegetation plan

--- lib/app/view_vegetation_plan/view/view_veg_plan_mobile.dart
+++ lib/app/view_vegetation_plan/view/view_veg_plan_mobile.dart
@@ -5,6 +5,7 @@ import "package:cmms/app/theme/dimens.dart";
 import "package:cmms/app/theme/styles.dart";
 import "package:cmms/app/view_vegetation_plan/view/equipment_bottomsheet.dart";
 import "package:cmms/app/view_vegetation_plan/view_veg_plan_controller.dart";
+import "package:cmms/app/widgets/view_veg_eqp_dialoag_mobile.dart";
 import "package:flutter/material.dart";
 import "package:get/get.dart";
 
@@ -202,49 +203,54 @@ class _ViewVegPlanMobileState extends State<ViewVegPlanMobile> {
                                                       )
                                                     ],
                                                   ),
-                                                  Row(//
-                                                      children: [
-                                                    Spacer(),
-                                                    GestureDetector(
-                                                      onTap: () {
-                                                        controller
-                                                                .selectedSchedules =
-                                                            scheduleModel;
-                                                        if (controller
-                                                                .selectedSchedules !=
-                                                            null) {
-                                                          showModalBottomSheet<
-                                                              void>(
-                                                            context: context,
-                                                            builder:
-                                                                (BuildContext
-                                                                    context) {
-                                                              return EquipmentBottomSheet();
-                                                            },
-                                                          );
-                                                        }
-                                                      },
-                                                      child: Container(
-                                                        padding: Dimens
-                                                            .edgeInsets8_2_8_2,
-                                                        decoration:
-                                                            BoxDecoration(
-                                                          color: ColorValues
-                                                              .viewColor,
-                                                          borderRadius:
-                                                              BorderRadius
-                                                                  .circular(4),
-                                                        ),
-                                                        child: Text(
-                                                          'View',
-                                                          style: Styles.white10
-                                                              .copyWith(
-                                                            color: Colors.white,
+                                                  Row(
+                                                    //
+                                                    children: [
+                                                      Spacer(),
+                                                      GestureDetector(
+                                                        onTap: () {
+                                                          controller
+                                                                  .selectedSchedules =
+                                                              scheduleModel;
+                                                          if (controller
+                                                                  .selectedSchedules !=
+                                                              null) {
+                                                            showModalBottomSheet<
+                                                                void>(
+                                                              context: context,
+                                                              builder:
+                                                                  (BuildContext
+                                                                      context) {
+                                                                return EquipmentBottomSheet();
+                                                              },
+                                                            );
+                                                          }
+                                                        },
+                                                        child: Container(
+                                                          padding: Dimens
+                                                              .edgeInsets8_2_8_2,
+                                                          decoration:
+                                                              BoxDecoration(
+                                                            color: ColorValues
+                                                                .viewColor,
+                                                            borderRadius:
+                                                                BorderRadius
+                                                                    .circular(
+                                                                        4),
+                                                          ),
+                                                          child: Text(
+                                                            'View',
+                                                            style: Styles
+                                                                .white10
+                                                                .copyWith(
+                                                              color:
+                                                                  Colors.white,
+                                                            ),
                                                           ),
                                                         ),
                                                       ),
-                                                    ),
-                                                  ]),
+                                                    ],
+                                                  ),
                                                 ],
                                               ),
                                             ),
@@ -253,6 +259,92 @@ class _ViewVegPlanMobileState extends State<ViewVegPlanMobile> {
                                       },
                                     ),
                                   ),
+                                  GestureDetector(
+                                    onTap: () {
+                                      var selectedEqp = [];
+                                      controller
+                                          .vegPlanDetailsModel.value!.schedules
+                                          ?.forEach((schedule) {
+                                        schedule.equipments?.forEach((element) {
+                                          var ee = element;
+                                          ee!.cleaningDay =
+                                              schedule.cleaningDay;
+                                          ;
+                                          selectedEqp.add(element);
+                                          print(element?.toJson());
+                                        });
+                                      });
+                                      selectedEqp.forEach((element) {
+                                        try {
+                                          var selectedParentIndex = controller
+                                              .vegEquipmentList
+                                              .indexWhere((eqp) =>
+                                                  eqp?.invId ==
+                                                  element.parentId);
+                                          print({
+                                            "selectedParentIndex":
+                                                selectedParentIndex
+                                          });
+                                          if (selectedParentIndex > -1) {
+                                            var selectedChildIndex = controller
+                                                    .vegEquipmentList[
+                                                        selectedParentIndex]!
+                                                    .smbs
+                                                    ?.indexWhere((smb) =>
+                                                        smb.smbId ==
+                                                        element.id) ??
+                                                -1;
+
+                                            if (selectedChildIndex > -1) {
+                                              var ss = controller
+                                                  .vegEquipmentList[
+                                                      selectedParentIndex]!
+                                                  .smbs?[selectedChildIndex];
+                                              ss?.selectedDay =
+                                                  "${element.cleaningDay}";
+                                              controller
+                                                      .vegEquipmentList[
+                                                          selectedParentIndex]!
+                                                      .smbs?[
+                                                  selectedChildIndex] = ss!;
+                                            }
+                                            print({
+                                              "selectedChildIndex":
+                                                  selectedChildIndex
+                                            });
+                                          }
+                                        } catch (e) {
+                                          print({"eadfds": e});
+                                        }
+                                      });
+                                      Get.dialog(
+                                          ViewVegSetEquipmentDialogMobile());
+                                    },
+                                    child: Container(
+                                      height: 30,
+                                      width: 150,
+                                      decoration: BoxDecoration(
+                                        color: ColorValues.addNewColor,
+                                        border: Border.all(
+                                          color: ColorValues
+                                              .lightGreyColorWithOpacity35,
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.all(
+                                            Radius.circular(5)),
+                                      ),
+                                      child: Center(
+                                        child: Text(
+                                          "Set Equipments",
+                                          style: TextStyle(
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w100,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  )
                                 ],
                               ),
                             ),
