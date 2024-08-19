eee9c22abb607e60f3d5d6b0169b64fce5270e42
madhubansahani <107603778+madhubansahani@users.noreply.github.com>
2024-08-19 14:33:17 +0530

    done

--- lib/app/list_of_Regulatary_Visits/regulataryvisits_list_controller.dart
+++ lib/app/list_of_Regulatary_Visits/regulataryvisits_list_controller.dart
@@ -1,5 +1,5 @@
 import 'package:cmms/app/home/home_controller.dart';
-import 'package:cmms/app/list_of_Regulatary_Visits%20/regulataryvisits_list_presenter.dart';
+import 'package:cmms/app/list_of_Regulatary_Visits/regulataryvisits_list_presenter.dart';
 import 'package:get/get.dart';
 
 class RegulataryDataListController extends GetxController {
