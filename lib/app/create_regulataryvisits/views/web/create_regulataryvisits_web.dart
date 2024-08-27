b65eba0adc2032f79747722d41c4c03f1a0b1936
Akash7309838 <108232093+Akash7309838@users.noreply.github.com>
2024-08-27 11:44:46 +0530

    routes change

--- lib/app/create_regulataryvisits/views/web/create_regulataryvisits_web.dart
+++ lib/app/create_regulataryvisits/views/web/create_regulataryvisits_web.dart
@@ -464,7 +464,7 @@ class _ViewRegulataryVisitsWebState extends State<RegulataryVisitsWeb> {
                             backgroundColor: ColorValues.cancelColor,
                             text: 'Cancel',
                             onPressed: () {
-                              Get.toNamed(Routes.complianceScreen);
+                              Get.toNamed(Routes.misDashboard);
                             },
                           ),
                         ),
