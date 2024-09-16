239eab93c4306efe7436ef74ca667abbf80c3836
madhubansahani <107603778+madhubansahani@users.noreply.github.com>
2024-09-16 14:17:50 +0530

    done the calender view

--- lib/app/calendar_view/calendar_view_presenter.dart
+++ lib/app/calendar_view/calendar_view_presenter.dart
@@ -1,7 +1,20 @@
+import 'package:cmms/domain/models/dashboard_model.dart';
 import 'package:cmms/domain/models/view_doc_upload.dart';
 import 'package:cmms/domain/usecases/calendar_view_usecase.dart';
 
 class CalendarViewPresenter {
   CalendarViewPresenter(this.calendarViewUsecase);
   CalendarViewUsecase calendarViewUsecase;
+  Future<List<DashboardModel?>?> getdashboardList({
+    String? facilityId,
+    dynamic endDate,
+    dynamic startDate,
+    bool? isLoading,
+  }) async =>
+      await calendarViewUsecase.getdashboardList(
+        facilityId: facilityId ?? "",
+        endDate: endDate,
+        startDate: startDate,
+        isLoading: isLoading ?? false,
+      );
 }
