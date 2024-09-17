9c548ab3e6982952cff4bfc1a880d1c9eea0dd3f
madhubansahani <107603778+madhubansahani@users.noreply.github.com>
2024-09-17 11:27:28 +0530

    done the vie wof calender vieew

--- lib/app/calendar_view/view/calendar_view_web.dart
+++ lib/app/calendar_view/view/calendar_view_web.dart
@@ -1,5 +1,9 @@
 import 'package:cmms/app/home/widgets/header_widget.dart';
+import 'package:cmms/app/navigators/app_pages.dart';
+import 'package:cmms/app/theme/color_values.dart';
+import 'package:cmms/app/theme/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:syncfusion_flutter_calendar/calendar.dart'; // Syncfusion Calendar
 
 class CalendarViewWeb extends StatelessWidget {
@@ -11,41 +15,90 @@ class CalendarViewWeb extends StatelessWidget {
         title: HeaderWidget(),
         elevation: 0.5,
       ),
-      body: Container(
-        width: double.infinity,
-        child: SfCalendar(
-          view: CalendarView.month, // Initial view
-          allowedViews: [
-            CalendarView.day,
-            CalendarView.week,
-            CalendarView.workWeek,
-            CalendarView.month,
-            CalendarView.timelineDay,
-            CalendarView.timelineWeek,
-            CalendarView.timelineWorkWeek,
-            CalendarView.timelineMonth
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 45,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Color.fromARGB(255, 227, 224, 224),
+                  width: 1,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 236, 234, 234).withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.home,
+                    color: ColorValues.greyLightColor,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Get.offNamed(Routes.home);
+                    },
+                    child: Text(
+                      "DASHBOARD",
+                      style: Styles.greyLight14,
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Get.offNamed(Routes.calendarViewDashbord);
+                    },
+                    child: Text(" / CALENDER", style: Styles.greyLight14),
+                  ),
+                  Text(" / CALENDER VIEW", style: Styles.greyLight14),
+                ],
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height,
+              child: SfCalendar(
+                view: CalendarView.month,
+                allowedViews: [
+                  CalendarView.day,
+                  CalendarView.week,
+                  CalendarView.workWeek,
+                  CalendarView.month,
+                  CalendarView.timelineDay,
+                  CalendarView.timelineWeek,
+                  CalendarView.timelineWorkWeek,
+                  CalendarView.timelineMonth
+                ],
+                dataSource: MeetingDataSource(_getDataSource()),
+                todayHighlightColor: Colors.blue,
+                showDatePickerButton: true,
+                showNavigationArrow: true,
+                monthViewSettings: MonthViewSettings(
+                  appointmentDisplayMode:
+                      MonthAppointmentDisplayMode.appointment,
+                  showAgenda: true,
+                ),
+                onTap: (CalendarTapDetails details) {
+                  print("Calendar tapped at date: ${details.date}");
+                  if (details.appointments != null &&
+                      details.appointments!.isNotEmpty) {
+                    final Meeting meeting = details.appointments!.first;
+                    print(
+                        "Tapped on meeting: ${meeting.eventName} from ${meeting.from} to ${meeting.to}");
+                  }
+                },
+                onViewChanged: (ViewChangedDetails details) {
+                  print(
+                      "View changed. Current visible dates: ${details.visibleDates}");
+                },
+              ),
+            ),
           ],
-          dataSource: MeetingDataSource(_getDataSource()),
-          todayHighlightColor: Colors.blue,
-          showDatePickerButton: true,
-          showNavigationArrow: true,
-          monthViewSettings: MonthViewSettings(
-            appointmentDisplayMode: MonthAppointmentDisplayMode.appointment,
-            showAgenda: true,
-          ),
-          onTap: (CalendarTapDetails details) {
-            print("Calendar tapped at date: ${details.date}");
-            if (details.appointments != null &&
-                details.appointments!.isNotEmpty) {
-              final Meeting meeting = details.appointments!.first;
-              print(
-                  "Tapped on meeting: ${meeting.eventName} from ${meeting.from} to ${meeting.to}");
-            }
-          },
-          onViewChanged: (ViewChangedDetails details) {
-            print(
-                "View changed. Current visible dates: ${details.visibleDates}");
-          },
         ),
       ),
     );
