5b2250724bffe41663b571cba715820b3a8e73f6
madhubansahani <107603778+madhubansahani@users.noreply.github.com>
2024-10-17 21:46:44 +0530

    done calender view status part

--- lib/domain/models/module_model.dart
+++ lib/domain/models/module_model.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
-List<ModuleModel> ModuleModelFromJson(String str) =>
-    List<ModuleModel>.from(
-        json.decode(str).map((x) => ModuleModel.fromJson(x)));
+List<ModuleModel> ModuleModelFromJson(String str) => List<ModuleModel>.from(
+    json.decode(str).map((x) => ModuleModel.fromJson(x)));
 
 String ModuleModelToJson(List<ModuleModel> data) =>
     json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
@@ -19,38 +18,38 @@ class ModuleModel {
   int? approve;
   int? issue;
   int? selfView;
+  int? software_id;
+  ModuleModel(
+      {this.id,
+      this.name,
+      this.moduleName,
+      this.menuImage,
+      this.add,
+      this.edit,
+      this.delete,
+      this.view,
+      this.approve,
+      this.issue,
+      this.selfView,
+      this.software_id});
 
-  ModuleModel({
-    this.id,
-    this.name,
-    this.moduleName,
-    this.menuImage,
-    this.add,
-    this.edit,
-    this.delete,
-    this.view,
-    this.approve,
-    this.issue,
-    this.selfView,
-  });
-
-  factory ModuleModel.fromJson(Map<String, dynamic> json) =>
-      ModuleModel(
-        id: json['id'],
-        name: json['featureName'],
-        moduleName: json['moduleName'],
-        menuImage: json['menuImage'],
-        add: json['add'],
-        edit: json['edit'],
-        delete: json['delete'],
-        view: json['view'],
-        approve: json['approve'],
-        issue: json['issue'],
-        selfView: json['selfView'],
-      );
+  factory ModuleModel.fromJson(Map<String, dynamic> json) => ModuleModel(
+      id: json['id'],
+      name: json['featureName'],
+      moduleName: json['moduleName'],
+      menuImage: json['menuImage'],
+      add: json['add'],
+      edit: json['edit'],
+      delete: json['delete'],
+      view: json['view'],
+      approve: json['approve'],
+      issue: json['issue'],
+      selfView: json['selfView'],
+      software_id: json['software_id']);
 
   Map<String, dynamic> toJson() => {
         "id": id,
+        "software_id": software_id,
         "featureName": name,
         "moduleName": moduleName,
         "menuImage": menuImage,
